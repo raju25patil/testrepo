baf352d9c234cf849e6a2432bdae4442b523374d
raju25patil <40533582+raju25patil@users.noreply.github.com>
2018-07-29 14:38:11 -0700

    Update dockerbuild.sh

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/local/bin/docker build -t atm .
+sudo /usr/local/bin/docker build -t atm .
 
-/usr/local/bin/docker push raju25/atm:latest
+sudo /usr/local/bin/docker push raju25/atm:latest
